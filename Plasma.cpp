8b3056b908a06117fc99779744994c32abc67023
Robert Atkins <ratkins@fastmail.fm>
2014-02-09 21:11:47 +0100

    Got a funkier plasma

--- Plasma.cpp
+++ Plasma.cpp
@@ -22,6 +22,9 @@ public:
             calcFrame(time);
         }
     }
+    //sin(distance(x, y, (128 * sin(-t) + 128), (128 * cos(-t) + 128)) / 40.74)
+    
+    // v = sin(10 * (x * sin(time / 2) + y * cos(time / 3)) + time)
     
     void calcFrame(int time) {
         for (int x = 0; x < width; x++) {
@@ -30,20 +33,30 @@ public:
                 
                 v += sin16(x * 1024 + time);
                 v += cos16(y * 2048 + time);
+                v += sin16(y * x * cos8(-time) / 2);
                 
-//                v = sin(10 *(x * sin(time / 2) + y * cos(time / 3)) + time)
                 
-//                v += sin16(128 * (512 * x * sin16(time / 128) + 128 * y * cos16(time / 512)) + time);
-                
+//                v += sin16((1024 * x * sin16(time) + 2048 * y * cos16(time)) + time);
+    
+//                v += sin16(1024 * distance(512 * x, 1024 * y, sin16(-time), cos16(-time)) / 128);
                 pixel(x, y) = CHSV((v >> 8) + 128, 255, 255);
             }
         }
         LEDS.show();
     }
     
+    int16_t distance(int x1, int y1, int x2, int y2) {
+        return sqrt((x2 - x1)^2 + (y1 - y2)^2);
+    }
+    
     uint8_t sin8(uint16_t x) {
         return (sin16(x) >> 8) + 128;
     }
+    
+    uint8_t cos8(uint16_t x) {
+        return (cos16(x) >> 8) + 128;
+    }
+
 };
 
 
