7d65a65617c2fdab43e845706599cc35d7436589
Robert Atkins <ratkins@fastmail.fm>
2014-02-09 21:25:34 +0100

    Now with added Super Plasma Wibble

--- Plasma.cpp
+++ Plasma.cpp
@@ -30,9 +30,9 @@ public:
         for (int x = 0; x < width; x++) {
             for (int y = 0; y < height; y++) {
                 int16_t v = 0;
-                
-                v += sin16(x * 1024 + time);
-                v += cos16(y * 2048 + time);
+                uint16_t wibble = sin8(time);
+                v += sin16(x * wibble * 8 + time);
+                v += cos16(y * (128 - wibble) * 4 + time);
                 v += sin16(y * x * cos8(-time) / 2);
                 
                 
