1877768bae8c4255321060bc8292da859e3a7e81
Robert Atkins <ratkins@fastmail.fm>
2014-02-17 19:59:50 +0100

    Before making any destructive changes to Life to try and make it 100% accurate.

--- Life.cpp
+++ Life.cpp
@@ -12,6 +12,7 @@ class Life : public Effect {
     
 private:
     uint8_t density;
+    uint16_t delta[36];
     
 public:
     
@@ -27,7 +28,7 @@ public:
     
     void start() {
         seed();
-        
+        uint8_t hue = 0;
         for (int time = 0; time < 128; time++) {
             for (int x = 0; x < width; x++) {
                 for (int y = 0; y < height; y++) {
@@ -38,7 +39,7 @@ public:
                         }
                     } else {
                         if (neighbours == 3) {
-                            pixel(x, y) = CHSV(0, 255, 255);
+                            pixel(x, y) = CHSV(hue++, 255, 255);
                         }
                     }
                 }
@@ -70,6 +71,19 @@ public:
     
     bool alive(int x, int y) {
         return inXRange(x) && inYRange(y) && pixel(x, y);
+    }
+    
+    // xxxx xxxx xxxx xxxx  xxxx xxxx xxxx xxxx
+    // xxxx xxxx xxxx xxxx  xxxx xxxx xxxx xxxx
+    
+    void setChanged(int x, int y) {
+        int index = y * 2;
+        if (x < 16) {
+            index += 0;
+        } else if (x < 32) {
+            index += 1;
+        }
+        delta[index] |= 1 << (x % 2);
     }
     
     void fadeout() {
